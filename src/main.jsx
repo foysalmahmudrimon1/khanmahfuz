8f981f28e1b4ad2ad8316429c35be0ada2c2c07f
MD. Foysal Mahmud Rimon <121936025+foysalmahmudrimon1@users.noreply.github.com>
2025-06-13 22:41:07 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom/client";
+
 import "./app.css";
 import { RouterProvider } from "react-router";
 import routes from "./Routes/Routes";
